018a2305c6076e6a955f6de85b0fb1eb5d5e0675
Michele de Olivio Correa <mcorrea@ciandt.com>
2020-07-27 09:51:42 -0300

    Revert "exemplo"
    This reverts commit 5041b8a9e94a51c0658ff61a640026f0e09c6bf0.

--- PaymentReponse.swift
+++ PaymentReponse.swift
@@ -4,7 +4,6 @@ class PaymentResponse: Codable {
     let documentValue: String
     let documentValue: Double
     let payerName: String
-    let campO: String
     
     enum CodingKeys: String, CodingKey {
         case documentValue = "valor_documento"
